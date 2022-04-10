7e940c505b99dc0a8419fbb9991bdcd012c80034
니꼬 <nico@nomadcoders.co>
2022-04-10 17:01:47 +0900

    3.4 Non Nullable Fields

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String
   }
   type Tweet {
     id: ID!
@@ -12,7 +14,7 @@ const typeDefs = gql`
   }
   type Query {
     allTweets: [Tweet!]!
-    tweet(id: ID!): Tweet!
+    tweet(id: ID!): Tweet
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
